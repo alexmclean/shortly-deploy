396b9bd8ffa19c60f602f3d386e4479346267ed1
Hack Reactor Students <accounts@hackreactor.com>
2015-11-05 16:08:08 -0800

    Running azure;

--- db/mongoConfig.js
+++ db/mongoConfig.js
@@ -6,7 +6,8 @@ var user = process.env.DBUSER || '';
 var password = process.env.DBPASSWORD || '';
 var database = process.env.DBDATABASE || 'shortlydb'
 
-mongoose.connect('mongodb://' + user + ':' + password + '@' + host + ':' + port + '/' + database);
+// mongoose.connect('mongodb://' + user + ':' + password + '@' + host + ':' + port + '/' + database);
+mongoose.connect('mongodb://MongoLab-z:GgRZQVMOnQg0Py1vKKhtJ3XbHCkLrlFMkDzzn4LyCRc-@ds052408.mongolab.com:52408/MongoLab-z')
 var db = mongoose.connection
 
 db.on('error', console.error.bind(console, "error connecting"));
